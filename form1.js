3dfd6702437d071c32c8193cfd8ef06aee9eebea
tharu <tharucse@gmail.com>
2022-09-12 17:45:48 -0500

    webform,style and js changes

---
+++ form1.js
@@ -0,0 +1,40 @@
+window.onload= function()
+{
+    document.getElementById("submit").onclick=validateFields;
+}
+
+function validateFields()
+{
+    var passwordRegex= /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{10,}$/;
+    var isValidpassword=document.getElementById("password").value.match(passwordRegex);
+
+    var urlRegex= /((http||https):\/\/\w{1,})$/;
+    var isValidUrl=document.getElementById("url").value.match(urlRegex);
+
+    if(!isValidUrl && !isValidpassword)
+    {
+        alert("Invalid password and url values are invalid");
+        return false;
+    }
+    else if(isValidUrl && !isValidpassword)
+    {
+        alert("Entered password is invalid");
+        return true;
+    }
+    else if(isValidUrl && !isValidpassword)
+    {
+        alert("Entered password is invalid");
+        return false;
+    }
+    if(!isValidUrl && isValidpassword)
+    {
+        alert("Invalid url");
+        return false;
+    }
+    else
+    {
+        alert("Entered password and url are valid");
+        return true;
+    }   
+       
+}
